e3b783abaee7a4be329bfe120da400d23cf9b6b9
Chester Cari <chester_cari@outlook.com>
2023-03-29 13:59:01 -0400

    added models and lights

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,19 +1,28 @@
-import { Grid, OrbitControls } from "@react-three/drei";
+import { Environment, Grid, OrbitControls, Stage } from "@react-three/drei";
 import { useControls } from "leva";
 import React from "react";
+import { Magikarp } from "./Magikarp";
+import { Model } from "./Magikarp2";
 
 type Props = {};
 
 const Experience = (props: Props) => {
-  const { scale } = useControls({ scale: -2 });
+  // const { color } = useControls({ color: "#b78135" });
   return (
     <>
       <OrbitControls />
       <Grid cellColor="white" args={[10, 10]} />
-      <mesh scale={scale}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial />
-      </mesh>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Environment preset="city" background={true} />
+      {/* <Stage
+        intensity={0.5}
+        environment="city"
+        shadows={{ type: "accumulative", color, colorBlend: 2, opacity: 2 }}
+        adjustCamera={0.9}
+      > */}
+      <Model />
+      {/* </Stage> */}
     </>
   );
 };
